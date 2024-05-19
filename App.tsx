16f7e127486e5198083a196da5f7a34131d2aec2
Kaique <skaique5424@gmail.com>
2024-05-18 22:35:36 -0300

    feito a tela de login

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Login from './src/Login';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+
+import { TEMAS } from './src/estilos/temas';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Te amo mamãe</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]}/>
+      <Login />
+    </NativeBaseProvider>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
