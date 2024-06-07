cdd6e57c4a3c061c52e60663e80fe6055d9512b0
Kaique <skaique5424@gmail.com>
2024-06-07 01:31:20 -0300

    telas de consulta e explorar feitas

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,9 +1,55 @@
-import { VStack, Text } from 'native-base'
+import { VStack, Text, Divider, ScrollView } from 'native-base'
+import { CardConsulta } from '../componentes/CardConsulta'
+import { Titulo } from '../componentes/Titulo'
+import { Botao } from '../componentes/Botao'
 
 export default function Consultas(){
   return(
-    <VStack>
-      <Text>Consultas</Text>
-    </VStack>
+    <ScrollView p="5">
+      <Titulo color="blue.500">Minhas consultas</Titulo>
+      <Botao mt={5} mb={5}>Agendar nova consulta</Botao>
+
+      <Titulo color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Próximas consultas</Titulo>
+
+      <CardConsulta
+        nome="Dr. Kaique"
+        especialidade='Cardiologista'
+        foto='https://github.com/kaique100.png'
+        data='20/20/2002'
+        foiAgendado
+       
+      />
+
+      <Divider mt={5}/>
+
+      <Titulo color="blue.500" fontSize="lg" alignSelf="flex-start" mb={2}>Consultas passadas</Titulo>
+
+      <CardConsulta
+        nome="Dr. Kaique"
+        especialidade='Cardiologista'
+        foto='https://github.com/kaique100.png'
+        data='20/20/2002'
+        foiAtendido
+       
+      />
+
+      <CardConsulta
+        nome="Dr. Kaique"
+        especialidade='Cardiologista'
+        foto='https://github.com/kaique100.png'
+        data='20/20/2002'
+        foiAtendido
+       
+      />
+
+        <CardConsulta
+          nome="Dr. Kaique"
+          especialidade='Cardiologista'
+          foto='https://github.com/kaique100.png'
+          data='20/20/2002'
+          foiAtendido
+        
+        />
+    </ScrollView>
   )
 }
